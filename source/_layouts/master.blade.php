089f49da437558df87d09a3d209ae86b64887941
Donnie Brandt <donniebrandt@icloud.com>
2016-06-05 13:52:06 -0400

    feat: twf design

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/css/main.css">
-    </head>
-    <body>
-        @yield('body')
-    </body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/main.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container">
+      @yield('body')
+    </div> <!-- /container -->
+  </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,28 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+<img src="images/forekast.svg" id="brand">
+
+<h1 class="title">The Weekly Forekast</h1>
+
+<p class="description">The easiest way to discover what's happening! Every Monday we send you a short summary of the best upcoming events.</p>
+
+<section class="subscribe">
+  <div class="form-group form-inline">
+    <input type="text" class="form-control" id="email" placeholder="your@email.com">
+    <button class="btn btn-primary" id="submit">Start discovering!</button>
+  </div>
+</section>
+
+<p class="how-it-works">How It Works: The <a href="https://www.forekast.com">Forekast</a> community post and votes on interesting upcoming events and we handpick the best ones and deliver them straight to your inbox!</p>
+
+<section class="syndication">
+  <img src="images/product-hunt.png">
+  <img src="images/reddit.png">
+  <img src="images/laughing-squid.png">
+  <img src="images/lifehacker.png">
+  <img src="images/awesomer.png">
+</section>
+
+<p class="sample">Still not convinced? Check out an <a href="http://us3.campaign-archive1.com/?u=9305570dd7f6062d4290cde95&id=d35d3d82d6&e=4caa488b08">example email</a>.</p>
 @endsection
